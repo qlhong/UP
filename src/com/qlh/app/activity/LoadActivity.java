80be5e842ae40c75d90d82e7946b1862dff1281d
qlhong <516967340@qq.com>
2016-02-01 11:18:13 +0800

    Update LoadActivity.java
    sadsa

--- src/com/qlh/app/activity/LoadActivity.java
+++ src/com/qlh/app/activity/LoadActivity.java
@@ -17,7 +17,7 @@ public class LoadActivity extends Activity {
 
 	protected void onCreate(Bundle paramBundle) {
 		super.onCreate(paramBundle);
-		System.out.println("111");
+		System.out.println("222");
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 	}
 
